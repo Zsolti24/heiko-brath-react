f4c0e9770cf6994c023683c42acaf8bfb958ab4d
Zsolti24 <tothzsolt2004@gmail.com>
2024-07-31 20:56:17 +0200

    CardSliderDone

--- src/sections/RecordSection.js
+++ src/sections/RecordSection.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination, Mousewheel, Keyboard, EffectCoverflow  } from 'swiper/modules';
+import { Navigation, Mousewheel, Keyboard } from 'swiper/modules';
 
 import 'swiper/css';
 import 'swiper/css/navigation';
@@ -63,29 +63,89 @@ export default function RecordSection() {
             <div className="RecommendationTitle">Was die Leute über uns sagen</div>
             <div className='swipers'>
             <Swiper
-                // cssMode={true}
+                cssMode={true}
                 modules={[Navigation, Mousewheel, Keyboard]}
-       
-                spaceBetween={50}
                 loop={true}
                 slidesPerView={3}
-                navigation
-                ={{
+                navigation={{
                     nextEl: '.swiper-button-next',
                     prevEl: '.swiper-button-prev',
                 }}
-                pagination={{ clickable: true }}
+                // pagination={{ clickable: true }}
                 simulateTouch={true}
+                className='CardSwiper'
+                breakpoints={{
+                    1200: {
+                        slidesPerView: 3,
+                        slidesPerColumn: 3
+                    },
+                    900: {
+                        slidesPerView: 1,
+                        slidesPerColumn: 1,
+                    },
+                    
+                    0: {
+                        slidesPerView: 1,
+                        slidesPerColumn: 1,
+                    },
+                  }}
                 >
-                <SwiperSlide>Slide 1</SwiperSlide>
-                <SwiperSlide>Slide 2</SwiperSlide>
-                <SwiperSlide>Slide 3</SwiperSlide>
-                <SwiperSlide>Slide 1</SwiperSlide>
-                <SwiperSlide>Slide 2</SwiperSlide>
-                <SwiperSlide>Slide 3</SwiperSlide>
+                    <SwiperSlide className='slideSmall'>
+                        <div className="card">
+                            <div className="message">Sprechen Sie deutsch aliquip ex ea commodo consequat. Wiener Schnitzel aute irure dolor in reprehenderit Guten Tag mollit anim Stuttgart.</div>
+                            <div className="sender">Maria Kartofeln</div>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide  className='slideBig'>
+                        <div className="card large">
+                            <img src={require("../images/grillkurs_icon.png")} alt="" />
+                            <img src={require("../images/zvezdice.png")}     alt="" />
+                            <div className="messageLarge">
+                                Wiener Schnitzel amet, consectetur Handtasche elit, sed do eiusmod tempor Stuttgart ut labore et dolore magna  Luftballons Ut Turnbeutel nostrud exercitation ullamco .
+                            </div>
+                            <img src={require("../images/znak.png")} alt="" className="znak" />
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide  className='slideSmall'>
+                        <div className="card">
+                            <div className="message">Achtung fur atine indoctum complectitur HugoClub Mate mea meliore denique nominavi id. Ohrwurm expetenda nam an, his ei Reise euismod assentior.</div>
+                            <div className="sender">Rene Weinstein</div>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide className='slideSmall'>
+                        <div className="card">
+                            <div className="message">Sprechen Sie deutsch aliquip ex ea commodo consequat. Wiener Schnitzel aute irure dolor in reprehenderit Guten Tag mollit anim Stuttgart.</div>
+                            <div className="sender">Maria Kartofeln</div>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide  className='slideBig'>
+                        <div className="card large">
+                            <img src={require("../images/grillkurs_icon.png")} alt="" />
+                            <img src={require("../images/zvezdice.png")} alt="" />
+                            <div className="messageLarge">
+                                Wiener Schnitzel amet, consectetur Handtasche elit, sed do eiusmod tempor Stuttgart ut labore et dolore magna  Luftballons Ut Turnbeutel nostrud exercitation ullamco .
+                            </div>
+                            <img src={require("../images/znak.png")} alt="" className="znak" />
+                            <img src={require("../images/znak.png")} alt="" className="znak" />
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide  className='slideSmall'>
+                        <div className="card">
+                            <div className="message">Achtung fur atine indoctum complectitur HugoClub Mate mea meliore denique nominavi id. Ohrwurm expetenda nam an, his ei Reise euismod assentior.</div>
+                            <div className="sender">Rene Weinstein</div>
+                        </div>
+                    </SwiperSlide>
                 </Swiper>
-                <div className="swiper-button-next SWBT">Next</div>
-                <div className="swiper-button-prev SWBT">Prev</div>
+                <div className='swpBtns'>
+                <div className='swpBtnsContainer'>
+                    <div className="swiper-button-next SWBT">
+                        <img src={require("../images/left.png")} alt="" className='NextBTN'/>
+                    </div>
+                    <div className="swiper-button-prev SWBT">
+                        <img src={require("../images/right.png")} alt="" className="PrevBTN" />
+                    </div>
+                </div>
+                </div>
             </div>
         </div>
     </div>
